aab17a67fcba2c3c1226c4b4f5473cb454e83c33
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-19 05:57:27 +0600

    practice different way using onclick

--- js/event.js
+++ js/event.js
@@ -2,3 +2,29 @@ function btn(){
     console.log("hello");
     
 }
+
+function makeYellow(){
+    document.body.style.backgroundColor = "yellow"
+}
+function makeRed(){
+    document.body.style.backgroundColor = "red"
+}
+
+const btnMakeBlue = document.getElementById("btn_make_blue")
+
+btnMakeBlue.onclick = function makeBlue(){
+    document.body.style.backgroundColor = "blue"
+}
+
+
+const btnMakePurple = document.getElementById("btn_make_purple")
+
+
+btnMakePurple.onclick = makePurple;
+
+function makePurple(){
+    document.body.style.backgroundColor = "purple"
+}
+
+
+
